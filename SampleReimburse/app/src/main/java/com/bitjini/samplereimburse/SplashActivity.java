ef105cd7a387ce8b72463ecc14608cd0262be527
spoorthi9095 <spoorthiraj095@gmail.com>
2017-02-23 16:52:27 +0530

    added nav bar

---
+++ SampleReimburse/app/src/main/java/com/bitjini/samplereimburse/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.bitjini.samplereimburse;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        CountDownTimer countDownTimer = new CountDownTimer(6000, 1000) {
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+
+                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }.start();
+
+
+    }
+}
